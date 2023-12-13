52bc4d61a3f67cc5bc9d98f97e49fc7b2fc50eaf
LNSY <lindsey.asterius@gmail.com>
2023-12-13 08:09:44 -0800

    wrote file-clerk tests, had one successful prompt for list-notebooks, all other tests are failing. Moving on to get prompting / scripting workflow tighter to try again.

--- tests.js
+++ tests.js
@@ -4,17 +4,17 @@ const app = require('express')();  // Assuming Express is properly set up in you
 const server = require('http').Server(app);  // Create an HTTP server using the Express app
 const routes = require('./routes.js')(app);
 
-
+global.root_directory = __dirname;
 
 const mocha = new Mocha({
-  timeout: 60000, // A one minute time out
+  timeout: 10000, // A one minute time out
   bail: false,   // bail on the first test failure
   reporter: 'spec', // choose a reporter (e.g., 'spec', 'nyan', 'dot', etc.)
 });
 
 
-mocha.addFile('./plugins/chat-gpt/chat-gpt.tests.js');
-// mocha.addFile('./plugins/file-clerk/file-clerk.tests.js');
+// mocha.addFile('./plugins/chat-gpt/chat-gpt.tests.js');
+mocha.addFile('./plugins/file-clerk/file-clerk.tests.js');
 
 mocha.run((failures) => {
   process.exitCode = failures ? 1 : 0; // exit with non-zero status if there are failures
